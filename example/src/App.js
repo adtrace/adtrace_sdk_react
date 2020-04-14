39bfc45dd4cda9eb128130540f95cc9ada289aaa
Aref <arefhosseini@yahoo.com>
2020-04-14 14:40:40 +0430

    change react example

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-adtrace'
+import AdTrace from 'react-adtrace'
 import 'react-adtrace/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+	const config = {
+  		app_token: '228fu1ygm2ta',
+  		environment: 'production', // or 'sandbox' in case you are testing SDK locally with your web app
+  		unique_id: '5056e25a-fh94-223o-b8a2-4ac4e20d48b2', // each web app user needs to have unique identifier,
+	}
+
+  return <AdTrace options = {config} />
 }
 
 export default App
